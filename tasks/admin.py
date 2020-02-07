3beb7ab0abf42f2270db57ea9f4fe2f1c12ac3f8
kingkiev <49226820+kingkiev@users.noreply.github.com>
2020-02-07 14:38:48 +0200

    Add files via upload

---
+++ tasks/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from tasks.models import TodoItem, Category, Priority
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('description', 'is_completed', 'created')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'todos_count')
+
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'todos_count')
+
